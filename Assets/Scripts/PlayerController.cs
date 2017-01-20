d54e61875af05fdc29957cf225194fc6b3f245e0
gurkenhobel <nilsw.agner@web.de>
2017-01-20 19:38:19 +0100

    Added playerController @ some config

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform _transform;
+
+    private InputDirection _input
+    {
+        get
+        {
+            return new InputDirection(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
+
+    
+}
---
+++ Assets/Scripts/Util/InputDirection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+struct InputDirection
+{
+    private float _x;
+    private float _y;
+
+    public float X
+    {
+        get
+        {
+            return _x;
+        }
+
+        set
+        {
+            _x = value;
+        }
+    }
+
+    public float Y
+    {
+        get
+        {
+            return _y;
+        }
+
+        set
+        {
+            _y = value;
+        }
+    }
+    public InputDirection(float x, float y)
+    {
+        _x = x;
+        _y = y;
+    }
+}
+
