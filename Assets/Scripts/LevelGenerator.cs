005c14a4f26739be01a77ba7c523ba3bea1f1898
theSoenke <soenke.behrendt@outlook.com>
2017-01-20 20:49:10 +0100

    Destroy old level parts

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,20 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject[] levelParts;
+    public List<GameObject> levelPrefabs;
     public float partWidth = 1;
 
     private float lastSpawnedPos;
     private int partsSpawned;
+    private readonly List<GameObject> spawnedParts = new List<GameObject>();
 
 
-    public void SetPlayerPosition(float xPos)
+    public void SetPlayerPosition(float xPlayerPos)
     {
-        if (lastSpawnedPos < xPos / partWidth)
+        if (lastSpawnedPos < xPlayerPos / partWidth)
         {
             float nextSpawnPos = lastSpawnedPos + (partsSpawned + partWidth);
             SpawnNewLevelPart(nextSpawnPos);
+        }
+        RemoveLevelsPartsOutOfView(xPlayerPos);
+    }
+
+    private void RemoveLevelsPartsOutOfView(float xPlayerPos)
+    {
+        GameObject levelPartToRemove = null;
+
+        foreach (var levelPrefab in spawnedParts)
+        {
+            if (levelPrefab.transform.position.x < xPlayerPos - partWidth)
+            {
+                levelPartToRemove = levelPrefab;
+            }
+        }
+
+        if (levelPartToRemove != null)
+        {
+            spawnedParts.Remove(levelPartToRemove);
+            Destroy(levelPartToRemove);
         }
     }
 
@@ -23,14 +45,15 @@ public class LevelGenerator : MonoBehaviour
         lastSpawnedPos = xPos;
         GameObject newLevelPart = GetNewLevelPart();
         var partPos = new Vector3(lastSpawnedPos, 0, 0);
-        var spawnedPart = Instantiate(newLevelPart, partPos, newLevelPart.transform.rotation);
+        GameObject spawnedPart = Instantiate(newLevelPart, partPos, newLevelPart.transform.rotation);
         spawnedPart.transform.SetParent(transform);
+        spawnedParts.Add(spawnedPart);
         partsSpawned++;
     }
 
     private GameObject GetNewLevelPart()
     {
-        int levelPartNum = Random.Range(0, levelParts.Length);
-        return levelParts[levelPartNum];
+        int levelPartNum = Random.Range(0, levelPrefabs.Count);
+        return levelPrefabs[levelPartNum];
     }
 }
