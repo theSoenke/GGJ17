4d79734264df7634814bd727decbca58f198b7ab
gurkenhobel <nilsw.agner@web.de>
2017-01-20 22:43:30 +0100

    gamecontroller

---
+++ Assets/Scripts/DieOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieOnCollision : MonoBehaviour
+{
+
+	private void OnTriggerEnter(Collider others)
+    {
+        Debug.Log("collision!");
+        GameController.Instance.GameOver(GameResult.Lose);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    private static GameController _instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GameOver(GameResult result)
+    {
+        switch(result)
+        {
+            case GameResult.Win:
+                Win();
+                break;
+            case GameResult.Lose:
+                Lose();
+                break;
+        }
+    }
+
+    private void Win()
+    {
+        //TODO: handle win state
+    }
+
+    private void Lose()
+    {
+        //TODO: handle lose state
+        _player.SetActive(false);
+    }
+}
