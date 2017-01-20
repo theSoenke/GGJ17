7d938da6af62be8fc4503ca2049644608cc57044
gurkenhobel <nilsw.agner@web.de>
2017-01-20 23:50:37 +0100

    added startmenu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _game;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.anyKey)
+        {
+            _game.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
