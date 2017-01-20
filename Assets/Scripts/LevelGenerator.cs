2ec18b9d1010743374471cb1813001334d67433f
theSoenke <soenke.behrendt@outlook.com>
2017-01-20 20:22:55 +0100

    Add initial level generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] levelParts;
+    public float partWidth = 1;
+
+    private float lastSpawnedPos;
+    private int partsSpawned;
+
+
+    public void SetPlayerPosition(float xPos)
+    {
+        if (lastSpawnedPos < xPos / partWidth)
+        {
+            float nextSpawnPos = lastSpawnedPos + (partsSpawned + partWidth);
+            SpawnNewLevelPart(nextSpawnPos);
+        }
+    }
+
+    private void SpawnNewLevelPart(float xPos)
+    {
+        lastSpawnedPos = xPos;
+        GameObject newLevelPart = GetNewLevelPart();
+        var partPos = new Vector3(lastSpawnedPos, 0, 0);
+        var spawnedPart = Instantiate(newLevelPart, partPos, newLevelPart.transform.rotation);
+        spawnedPart.transform.SetParent(transform);
+        partsSpawned++;
+    }
+
+    private GameObject GetNewLevelPart()
+    {
+        int levelPartNum = Random.Range(0, levelParts.Length);
+        return levelParts[levelPartNum];
+    }
+}
---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    public LevelGenerator levelGenerator;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            levelGenerator.SetPlayerPosition(transform.position.x);
+        }
+    }
+}
