84400d0c5dc123abe1093636c77061c490997145
theSoenke <soenke.behrendt@outlook.com>
2017-01-22 11:48:19 +0100

    Mine blink effect

--- Assets/Scripts/Objects/Mine.cs
+++ Assets/Scripts/Objects/Mine.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public float blinkTime = 1;
 
+    private Renderer mineRenderer;
+
+
+    private void Awake()
+    {
+        mineRenderer = GetComponent<Renderer>();
+    }
 
     private void OnTriggerEnter(Collider col)
     {
@@ -11,5 +20,22 @@ public class Mine : MonoBehaviour
         GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         Destroy(explosion, 5);
         GameController.Instance.HitMine();
+    }
+
+    public void ShowMine()
+    {
+        StartCoroutine(Blink(blinkTime));
+    }
+
+    private IEnumerator Blink(float duration)
+    {
+        float endTime = Time.time + duration;
+        while (Time.time < endTime)
+        {
+            mineRenderer.enabled = false;
+            yield return new WaitForSeconds(0.2f);
+            mineRenderer.enabled = true;
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
